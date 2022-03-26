b1b7034b79b075f2c8f594ce5ad4d19f94c979e2
Christopher Pelloux <git@chp.io>
2022-03-25 21:15:17 -0400

    Apply Harshad's changes for rust-vmm
    Signed-off-by: Christopher Pelloux <git@chp.io>

--- shim/linux/src/entry.c
+++ shim/linux/src/entry.c
@@ -276,10 +276,12 @@ dispatch_system_kvm_get_msr_index_list(
         return -EINVAL;
     }
 
-    if (mut_args.nmsrs > MSR_LIST_MAX_INDICES) {
-        bferror("caller nmsrs exceeds MSR_LIST_MAX_INDICES");
-        return -ENOMEM;
-    }
+    //bfdebug_x32("mut_args.nmsrs",mut_args.nmsrs);
+
+    // if (mut_args.nmsrs > MSR_LIST_MAX_INDICES) {
+    //     bferror("caller nmsrs exceeds MSR_LIST_MAX_INDICES");
+    //     return -ENOMEM;
+    // }
 
     mut_ret = handle_system_kvm_get_msr_index_list(&mut_args);
     if (SHIM_2BIG == mut_ret) {
@@ -556,7 +558,6 @@ static long
 dispatch_vm_kvm_create_pit2(
     struct shim_vm_t *const pmut_vm, struct kvm_pit_config *const user_args)
 {
-    uint32_t ret;
     struct kvm_pit_config mut_args;
     uint64_t const size = sizeof(mut_args);
 
@@ -565,11 +566,11 @@ dispatch_vm_kvm_create_pit2(
         return -EINVAL;
     }
 
-    if (handle_vm_kvm_create_pit2(pmut_vm, user_args)) {
+    if (handle_vm_kvm_create_pit2(pmut_vm, &mut_args)) {
         bferror("handle_vm_kvm_create_pit2 failed");
         return -EINVAL;
     }
-    return (long)ret;
+    return 0;
 }
 
 static long
@@ -1340,17 +1341,20 @@ dispatch_vcpu_kvm_run(struct shim_vcpu_t *const vcpu)
     switch (handle_vcpu_kvm_run(vcpu)) {
         case SHIM_SUCCESS: {
             mut_ret = 0;
+            bferror_x32("kvm_run: SHIM_SUCCESS ,DELTA", mut_ret);
             break;
         }
 
         case SHIM_INTERRUPTED: {
             mut_ret = -EINTR;
+            bferror_x32("kvm_run: SHIM_INTERRUPTED ,DELTA", mut_ret);
             break;
         }
 
         default: {
             bferror("handle_vcpu_kvm_run failed");
             mut_ret = -EINVAL;
+            bferror_x32("kvm_run: Default ,DELTA", mut_ret);
             break;
         }
     }
